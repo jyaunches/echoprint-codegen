6e2f50b43304c14b6c3cbeba3229834b09e6bcd4
jyaunches <jmyaunch@gmail.com>
2013-10-04 09:24:25 -0400

    Julie: updating project to iOS7, adding user files and build files to git ignore

--- src/echoprint-codegen-ios/build/echoprint-codegen-ios.build/Debug-iphoneos/echoprint-codegen-ios.build/Objects-normal/armv7/Base64.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Tlacael/Developer/echoprint-codegen/src/echoprint-codegen-ios/echoprint-codegen-ios/../../Base64.cxx \
-  /Users/Tlacael/Developer/echoprint-codegen/src/echoprint-codegen-ios/echoprint-codegen-ios/../../Base64.h
--- src/echoprint-codegen-ios/build/echoprint-codegen-ios.build/Debug-iphoneos/echoprint-codegen-ios.build/Objects-normal/armv7s/AudioStreamInput.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Tlacael/Developer/echoprint-codegen/src/echoprint-codegen-ios/echoprint-codegen-ios/../../AudioStreamInput.cxx \
-  /Users/Tlacael/Developer/echoprint-codegen/src/echoprint-codegen-ios/echoprint-codegen-ios/../../AudioStreamInput.h \
-  /Users/Tlacael/Developer/echoprint-codegen/src/echoprint-codegen-ios/echoprint-codegen-ios/../../Common.h \
-  /Users/Tlacael/Developer/echoprint-codegen/src/echoprint-codegen-ios/echoprint-codegen-ios/../../Params.h \
-  /Users/Tlacael/Developer/echoprint-codegen/src/echoprint-codegen-ios/echoprint-codegen-ios/../../File.h
